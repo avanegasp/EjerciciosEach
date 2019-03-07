22d906c0845f7b4be130ea0f88d0e2c64e515cc0
Angie <arco527@gmail.com>
2019-03-06 19:35:11 -0500

    Exercises makeitreal each

---
+++ prueba.rb
@@ -0,0 +1,9 @@
+#EJERCICIOS
+
+#Ingresa el nombre de todos los compañeros de clase y cambialos de orden por medio de un método.
+#En una línea muestra cada estudiante del curso
+#Ingresa un nuevo estudiante al arreglo de estudiantes
+#Reemplaza el nombre de la posición 3 por "Miami"
+#Elimina el último nombre
+#Elimina los últimos dos
+#Enumera cada estudiante, nombralo y dí en que index esta 
